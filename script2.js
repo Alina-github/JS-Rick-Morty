eadaad63aaac6689f710198d472d86493d296587
Alina-github <alinarudkovskaa@gmail.com>
2021-04-19 17:58:03 +0300

    refactor: improve the code across all the files

--- script2.js
+++ script2.js
@@ -1,50 +1,71 @@
 const container = document.querySelector(".content__column");
-let idArray = Array.from({ length: 671 }, (value, number) => number + 1);
-let id = 0;
-const cardLimit = 10;
-let arrayOfCharacters;
+const loader = document.querySelector(".loading");
+let idArray = [];
+let id = 1;
+let cardsLimit = 10;
 
-loadCharacters();
-
-async function loadCharacters() {
-  try {
-    let response = await fetch(`https://rickandmortyapi.com/api/character/${idArray}`);
-    let data = await response.json();
-    arrayOfCharacters = data;
-    getArticle(arrayOfCharacters);
-  } catch (err) {
-    console.error(err.message);
-  }
-}
-
-function getArticle(arrayOfCharacters) {
-  for (let i = 0; i < cardLimit; i++) {
+const getRangeofCharacters = (id) => {
+  for (let i = 0; i < cardsLimit; i++) {
+    idArray.push(id);
     id++;
-    console.log(arrayOfCharacters);
-    addAtricletoDOM(id, arrayOfCharacters)
+  }
+  return idArray;
+}
+
+async function loadCharacters(id) {
+  try {
+    let response = await fetch(`https://rickandmortyapi.com/api/character/${getRangeofCharacters(id)}`);
+    let data = await response.json();
+    return Promise.resolve(data);
+  } catch (err) {
+    console.error(Promise.rejected(data));
   }
 }
 
-function addAtricletoDOM(id, array) {
+const showArticles = () => loadCharacters(id).then(data => {
+  data.forEach(character => addAtricletoDOM(character));
+});
+
+function addAtricletoDOM(character) {
   let postArticle = document.createElement('article');
   postArticle.classList.add('content__item');
   postArticle.innerHTML =
     `<div class="item__image">
-   <img src=${array[id].image} alt="${array[id].name}">
+   <img src=${character.image} alt="${character.name}">
   </div>
   <div class="item__info">
-    <h2>${array[id].name}</h2>
-    <h3>Status: ${array[id].status}<h3>
+    <h2>${character.name}</h2>
+    <h3>Status: ${character.status}<h3>
   <div>
-    <p> Last known location: <b>${array[id].location.name}</b></p>
+    <p> Last known location: <b>${character.location.name}</b></p>
   </div>`;
   container.appendChild(postArticle);
+}
+
+function showLoading() {
+  loader.classList.add('show');
+}
+
+function hideLoading() {
+  loader.classList.remove('show');
+}
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
 }
 
 window.addEventListener('scroll', function () {
   const rootElement = document.documentElement;
   const { scrollTop, scrollHeight, clientHeight } = rootElement;
-  if (scrollTop + clientHeight >= scrollHeight - 1) {
-    getArticle(arrayOfCharacters);
+  if (scrollTop + clientHeight >= scrollHeight) {
+    idArray = [];
+    id += cardsLimit;
+    showLoading();
+    setTimeout(() => {
+      showArticles();
+      hideLoading();
+    }, 2000);
   }
 })
+
+showArticles();
